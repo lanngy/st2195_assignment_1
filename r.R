dd3ab3b62eddd64121603a5ea3540c41464f6034
Lan <lan.nguy@outlook.com>
2021-11-18 18:33:18 +0100

    R-Script assignment

---
+++ r.R
@@ -0,0 +1,33 @@
+
+a <- 1
+print (a)
+
+b <- 2
+print (b)
+
+c <- 3
+print (c)
+
+d <- 4
+print (d)
+
+e <- 5
+print (e)
+
+f <- 6
+print (f)
+
+g <- 7
+print (g)
+
+h <- 8
+print (h)
+
+i <- 9
+print (i)
+
+k <- 10
+print (k)
+
+
+
